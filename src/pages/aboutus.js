e330dde9cb4f87e76558d550a6174099e616f69e
Franco Pacifico <francogpacifico24@gmail.com>
2022-10-25 18:04:58 +0800

    added content for about us page and gave the footer bootstrap

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import aboutimg from '../images/aboutsus.png'
+import './Aboutus.css'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Button from '@mui/material/Button';
+
+
+const StyledLink = styled(Link)`
+    color: white;
+    text-decoration: none;
+    margin: 1rem;
+    position relative;
+    &:hover, &:focus {
+      color: yellow;
+    }
+    &:active{
+      color: red;
+    }
+  `;
 
 const Aboutus = () => {
   return (
     <div>
-      <h1>About Us page</h1>
+      <div className="torow">
+        <div>
+          <img src={aboutimg} style={{maxWidth:"500px"}}></img>  
+        </div>
+        <div>
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, itaque eius fugiat quo saepe quasi voluptatum, quas ea, numquam quisquam sunt! At, itaque eius fugiat quo saepe quasi voluptatum, quas ea, numquam quisquam sunt! At, itaque eius fugiat quo saepe quasi voluptatum, quas ea, numquam quisquam sunt! At, itaque eius fugiat quo saepe quasi voluptatum, quas ea, numquam quisquam sunt! Eveniet minus deleniti in nesciunt voluptatem ratione sit! Optio ut porro voluptates magnam commodi dicta tenetur cupiditate, necessitatibus nobis est inventore nisi nemo perferendis quidem adipisci iste suscipit error.</p>
+          <StyledLink to="/login"><Button>See Developers</Button></StyledLink>
+        </div>
+      </div>
+      
     </div>
   )
 }
