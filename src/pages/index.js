fb5e37fe359f03b6298728e425fc71e3b3a357fa
John Harold Dela cruz <jhdlc12@gmail.com>
2022-10-27 17:25:00 +0800

    added the home page and find a job page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from '../components/Header'
-import Home from './home'
-import Findjob from './findjob'
-import Aboutus from './aboutus'
-import Login from './login'
-import Contactus from './contactus'
-import Footer from '../components/Footer'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Home from "./home";
+import Findjob from "./findjob";
+import Aboutus from "./aboutus";
+import Login from "./login";
+import Contactus from "./contactus";
+import Footer from "../components/Footer";
 
 const Index = () => {
   return (
     <Router>
-        <Header/>
-        <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route exact path='/findjob' element={<Findjob/>}/>
-            <Route exact path='/aboutus' element={<Aboutus/>}/>
-            <Route exact path='/contactus' element={<Contactus/>}/>
-            <Route exact path='/login' element={<Login/>}/>
-
-        </Routes>
-        <Footer/>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/findjob" element={<Findjob />} />
+        <Route exact path="/aboutus" element={<Aboutus />} />
+        <Route exact path="/contactus" element={<Contactus />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
